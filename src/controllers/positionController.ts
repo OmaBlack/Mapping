30c3152c87804e90872704080f01473d13dd1542
durotemy <durotemy@gmail.com>
2022-06-14 01:14:11 +0100

    mapLongLat

--- src/controllers/positionController.ts
+++ src/controllers/positionController.ts
@@ -1,20 +1,13 @@
-import express from "express";
-import dotenv from "dotenv";
-// require('dotenv').config()
-dotenv.config();
-import axios from "axios";
 import { Request, Response } from "express";
-const apiKey = process.env.KEY;
-const tomtomKey = process.env.TOMTOM;
-console.log("api", tomtomKey);
+import dotenv from "dotenv";
+import axios from "axios";
+
 export const getLatitudeAndLongitude = async (req: Request, res: Response) => {
+  const tomtomKey = process.env.KEY;
   const { key } = req.body;
-  console.log("tomtom", apiKey);
-  console.log("query here", req.query);
-  
   try {
     const data = await axios.get(
-      `https://api.tomtom.com/search/2/geocode/${key}${apiKey}`
+      `https://api.tomtom.com/search/2/geocode/${key}${tomtomKey}`
     );
     const result = data.data.results;
     let storeArray: number[] = [];
@@ -34,11 +27,3 @@ export const getLatitudeAndLongitude = async (req: Request, res: Response) => {
     res.json({ error });
   }
 };
-
-// const data2 = await axios.get("https://api.tomtom.com/search/2/geocode/2a%2C%20Louis%20Solomon%20street%2C%20Lagos%2C%20Nigeria.json?key=lnbGQ5NgbNlQfuGmwOo7YiI2Gc3YipAX");
-//   console.log(data2.data);
-//   res.send(data2.data);
-
-// const data = await axios.get(
-//   "https://api.tomtom.com/search/2/geocode/2a%2C%20Louis%20Solomon%20street%2C%20Lagos%2C%20Nigeria.json?key=lnbGQ5NgbNlQfuGmwOo7YiI2Gc3YipAX"
-// );
